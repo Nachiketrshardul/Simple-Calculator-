b444772b7c25a9888fa1c76aa15f706078e423e6
Nachiketrshardul <rshardul699@gmwil.com>
2025-08-04 15:08:14 +0530

    Updated Simple_calculator.cpp

---
+++ Simple_calculator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2;
+
+    cout << "Enter operator (+, -, *, /, %): ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2;
+            else
+                cout << "Error! Division by zero.";
+            break;
+        case '%':
+            if((int)num2 != 0)
+                cout << (int)num1 << " % " << (int)num2 << " = " << (int)num1 % (int)num2;
+            else
+                cout << "Error! Division by zero.";
+            break;
+        default:
+            cout << "Invalid operator!";
+            break;
+    }
+
+    return 0;
+}
